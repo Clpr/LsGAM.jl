90fd2b46888011a8c8893fa5e6bc9f97623f29d5
Clpr <zhao1tian1hao1@outlook.com>
2025-05-29 14:11:46 +0800

    add InvExponential

--- src/terms/exp.jl
+++ src/terms/exp.jl
@@ -39,3 +39,50 @@ function ∂2(g::Exponential, x::AbstractVector)::Vector{Matrix{Float64}}
     end
     return res
 end
+
+
+
+
+
+# ==============================================================================
+"""
+    InvExponential(; degrees::Vector{Int} = [1,])
+
+InvExponential terms of each element of `x` vector.
+
+- Input size : `x` ∈ R^n
+- Output size: R^n
+
+Allocation:
+
+[exp(-x[1]), exp(-x[2]), ...]
+
+Attention: the returned Jacobian and Hessian are DENSE. The sparse version may
+come in the future.
+"""
+struct InvExponential <: AbstractTerm
+    function InvExponential()
+        new()
+    end
+end
+# ------------------------------------------------------------------------------
+function Base.length(g::InvExponential, n::Int)::Int
+    return n
+end
+# ------------------------------------------------------------------------------
+function (g::InvExponential)(x::AbstractVector)::Vector{Float64}
+    return exp.(-x)
+end
+# ------------------------------------------------------------------------------
+function ∂(g::InvExponential, x::AbstractVector)::Matrix{Float64}
+    return diagm(-exp.(-x))
+end
+# ------------------------------------------------------------------------------
+function ∂2(g::InvExponential, x::AbstractVector)::Vector{Matrix{Float64}}
+    n = length(x)
+    res = Matrix{Float64}[zeros(Float64, n, n) for _ in 1:n]
+    for i in 1:n
+        res[i][i, i] = exp(-x[i])
+    end
+    return res
+end
